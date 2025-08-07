5ef5c2bc81e0c77347df527309a8a06d54cf07b5
Inoth <115546699+Inothhh@users.noreply.github.com>
2025-08-07 09:45:08 +0200

    Delete module_bi directory

--- module_bi/projet_Grenoble_Invest_Alpes/projet_grenoble/models/staging/stg_nomtable.sql
+++
@@ -1,7 +0,0 @@
--- models/staging/stg_base_entreprises.sql
-select
-  siret,
-  société as nom_societe,
-  cast(effectifs_2024 as integer) as nb_effectifs,
-  lower(commune) as commune_normalisée
-from {{ source('raw', 'base_entreprises') }}
